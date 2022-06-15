583dc5e20a63707fb36c657b6c2e82e77a3d1dc4
Marcelo Machado <mjoelmachadojr@gmail.com>
2022-06-15 10:22:08 -0300

    first commit

---
+++ src/assets/js/login.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('.login__input');
+const button = document.querySelector('.login__button');
+const form = document.querySelector('.login__form');
+
+const validateInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    } 
+        button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
